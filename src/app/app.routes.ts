e38a906d1193f72f2c4f2399221db1c7873b0837
Ahmed Salama <ahmedsalamafcis@gmail.com>
2026-03-16 18:41:15 +0200

    updating the Humberger menu, updated the menu to be arabic in the arabic view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,13 @@ import { Home } from './pages/home/home';
 import { Dashboard } from './pages/dashboard/dashboard';
 import { ProjectDetails } from './pages/project-details/project-details';
 import { AllProjects } from './pages/all-projects/all-projects';
+import { Login } from './pages/login/login';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'admin', component: Dashboard },
   { path: 'project/:id', component: ProjectDetails },
   { path: 'all-projects', component: AllProjects },
+  { path: 'login', component: Login },
+  { path: '**', redirectTo: '' }, // Redirect any unknown paths to home
 ];
